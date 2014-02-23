d466bbdf7cf1b6bb55f22aa1dd0ba0c56b73743c
bjspencer <brianspencer@outlook.com>
2014-02-22 22:02:51 -0700

    Added the ability to programatically list and select BYU buildings in Map

---
+++ Map/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map
+{
+    static class Constants
+    {
+        public const string ByuVenueId = "hcl-brighamyounguniversity";
+        public const string StadiumVenueId = "hcl-lavelledwardsstadium";
+    }
+}
