4cb39cfda89574310d7e1781f6dfa98757c88d52
jeffrpowell <jeff.powell@byu.edu>
2014-03-11 16:47:01 -0600

    Added reference to our other projects in TestSuite

---
+++ BYU/TestSuite/Common/WebServices/CamelCaseNameMatchingConverterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Common.WebServices;
+
+namespace TestSuite
+{
+    [TestClass]
+    public class ClassesTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
