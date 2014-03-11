c488f74239bb1b55c4a1529ee52c79dad7875854
mikebcurtis <mikebcurtis@gmail.com>
2014-03-11 17:26:26 -0600

    private method testing example

--- BYU/TestSuite/BYU/HomeScreen/HomeScreenTest1.cs
+++ BYU/TestSuite/BYU/HomeScreen/HomeScreenTest1.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+// Use the following to unit test private methods.
+// If you don't alias the following using, it will conflict with the unit test framework.
+using TestTools = Microsoft.VisualStudio.TestTools.UnitTesting;
+using BYU;
 
 namespace TestSuite
 {
@@ -18,16 +22,17 @@ namespace TestSuite
 
         [TestMethod]
         public void Login()
-        { 
+        {
+            HubPage page = new BYU.HubPage();
+            TestTools.PrivateObject obj = new TestTools.PrivateObject(page);
+            string netId = "asdf"; // TODO need a test user/pass
+            string pass = "asdf";
+            obj.Invoke("DoLogin",new string[] {netId, pass});
+            // TODO assert login succeeded
         }
 
         [TestMethod]
         public void Logout()
-        {
-        }
-
-        [TestMethod]
-        public void Login()
         {
         }
     }
