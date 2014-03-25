4ca515c2eb626e2d8e9e8d55b61dd8d369f01e57
mikebcurtis <mikebcurtis@gmail.com>
2014-03-25 17:49:05 -0600

    initial live tile background task project

---
+++ BYU/BackgroundTask/LiveTileBackgroundTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Windows.Web.Syndication;
+
+namespace BackgroundTask
+{
+    public sealed class LiveTileBackgroundTask : IBackgroundTask
+    {
+        // TODO add a new background task to the package manifest once something's implemented here
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
