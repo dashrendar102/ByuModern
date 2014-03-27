39c635bebcfa58bab92cd5a417f44667fec94a55
dashrendar102 <dashrendar102@hotmail.com>
2014-03-26 22:11:06 -0600

    Removed unused 'using' statements

--- BYU/BYU/Map/ByuMap.xaml.cs
+++ BYU/BYU/Map/ByuMap.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using Bing.Maps;
 using Bing.Maps.VenueMaps;
-using Common;
 using Common.Storage;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
